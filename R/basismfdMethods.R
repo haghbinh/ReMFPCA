bc6393063dfa94d2b2d4194ff5e3f8bc8c08e932
haghbinh <haghbinh@gmail.com>
2022-06-19 00:40:59 +0430

    Adding 2d basises

--- R/basismfdMethods.R
+++ R/basismfdMethods.R
@@ -1,9 +1,89 @@
-### ==============Support dimension method for the basismfd object =========
+### ==============Evaluation method for the basismfd object =========
+#'  Values of basismfd Basis Functions 
+#'
+#' Computes the basis matrix evaluated at arguments in EVALARG associated with basismfd object 
+#' @param evalarg a list of numeric vectors of argument values at which the \code{basismfd} is to be evaluated.
+#' @param basisobj  an object of class \code{basismfd} defining basis functions whose values are to be computed.
+#' @return a matrix of basis function values with rows corresponding to argument values and columns to basis functions.
+#' @export evalbasismfd
+#' @examples
+#' x <- 1
+#' 
+setGeneric("evalbasismfd", function(evalarg, basisobj) {standardGeneric("evalbasismfd")})
+
+#' evalbasismfd of basismfd objects
+#' @importFrom fda eval.basis
+#' @keywords internal
+setMethod("evalbasismfd",
+          signature = c(evalarg = "list", basisobj = "basismfd"),
+          function(evalarg, basisobj) {
+            s <- basisobj@support
+            p <- basisobj@p
+            B <- basisobj@B
+            d <- dimSupp(basisobj)
+            if (length(evalarg) != p) {
+              stop(paste("The length of evalarg must be ", p, " ."))
+            }
+            out <- list()
+            for (i in 1:p) {
+              if(is.basis(B[[i]])){
+               out[[i]] <- eval.basis(evalarg[[i]],B[[i]])
+              }else{
+                if(inherits(B[[i]],"basisEmp")){
+                  out[[i]] <- B[[i]]@Basis
+                } else{
+                  b1 <- eval.basis(evalarg[[i]][[1]],B[[i]]@bs1)
+                  b2 <- eval.basis(evalarg[[i]][[2]],B[[i]]@bs2)
+                  out[[i]] <- kronecker(b1,b2)
+                }
+                
+              }
+            }
+            return(out)
+          }
+)
+
+
+
+
+#' evalbasismfd of basismfd objects
+#' @importFrom fda eval.basis
+#' @keywords internal
+setMethod("evalbasismfd",
+          signature = c(evalarg = "numeric", basisobj = "basismfd"),
+          function(evalarg, basisobj) {
+            s <- basisobj@support
+            p <- basisobj@p
+            B <- basisobj@B
+            d <- dimSupp(basisobj)
+            if (p > 1) {
+              warning("The argument vector is applid for all basises")
+            }
+            out <- list()
+            for (i in 1:p) {
+              if(is.basis(B[[i]])){
+                out[[i]] <- eval.basis(evalarg,B[[i]])
+              }else{
+                if(inherits(B[[i]],"basisEmp")){
+                  out[[i]] <- B[[i]]@Basis
+                } else{
+                  b1 <- eval.basis(evalarg,B[[i]]@bs1)
+                  b2 <- eval.basis(evalarg,B[[i]]@bs2)
+                  out[[i]] <- kronecker(b1,b2)
+                }
+                
+              }
+            }
+            return(out)
+          }
+)
+
+
 #' Support dimension of functional data
 #'
 #' This function returns the support dimension of an object of class  \code{basismfd}.
 #' @param object An object of  class \code{mfd}.
-#'  @return a numeric vector, giving the support dimension of each variable.
+#' @return a numeric vector, giving the support dimension of each variable.
 #' @export dimSupp
 #' @examples
 #' x <- 1
@@ -19,43 +99,3 @@ setMethod("dimSupp", signature = "basismfd",
             vapply(s, FUN = ncol, FUN.VALUE = 0)})
 
 
-
-
-
-### ==============Evaluation method for the basismfd object =========
-#'  Values of basismfd Basis Functions 
-#'
-#' Computes the basis matrix evaluated at arguments in EVALARG associated
-#' with basismfd object 
-
-#' @param evalarg a list of numeric vectors of argument values at which the \code{basismfd} is to be evaluated.
-#' @param basisobj  an object of class \code{basismfd} defining basis functions whose values are to be computed.
-#'  @return a matrix of basis function values with rows corresponding to argument values and columns to basis functions.
-#' @export eval.basismfd
-#' @examples
-#' x <- 1
-#' 
-
-setGeneric("eval.basismfd", function(evalarg, basisobj) {standardGeneric("eval.basismfd")})
-
-#' evalvaluation of basismfd objects
-#'
-#' @keywords internal
-setMethod("eval.basismfd",
-  signature = c(evalarg = "list", basisobj = "basismfd"),
-  function(evalarg, basisobj) {
-    s <- basisobj@support
-    p <- basisobj@p
-    B <- basisobj@B
-    d <- dimSupp(basisobj)
-    if (length(evalarg) != p) {
-      stop(paste("The length of evalarg must be ", p, " ."))
-    }
-
-    out <- list()
-    for (i in 1:p) {
-      out <- ifelse(is.basis(B[[i]]), eval.basis(evalarg[[i]]), B[[i]]@Basis)
-    }
-    return(out)
-  }
-)
